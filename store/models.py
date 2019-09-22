3f3d26f1c13daa35804c1c02d060b69bf2c04088
Stéphane Metz <condor.webdev@gmail.com>
2019-09-22 16:47:56 +0200

    rewrite __str__ method

--- store/models.py
+++ store/models.py
@@ -1,13 +1,22 @@
 from django.db import models
 
 class Artist(models.Model):
+    def __str__(self):
+        return self.name
+
     name = models.CharField(max_length=200, unique=True)
 
 class Contact(models.Model):
+    def __str__(self):
+        return self.name
+
     email = models.EmailField(max_length=100, unique=True)
     name = models.CharField(max_length=200)
 
 class Album(models.Model):
+    def __str__(self):
+        return self.title
+
     reference = models.IntegerField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     available = models.BooleanField(default=True)
@@ -17,6 +26,9 @@ class Album(models.Model):
 
 
 class Booking(models.Model):
+    def __str__(self):
+        return self.contact.name
+
     created_at = models.DateTimeField(auto_now_add=True)
     contacted = models.BooleanField(default=False)
     album = models.OneToOneField(Album)
