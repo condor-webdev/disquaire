cf1ec9416897171722e034cff412274901ec64c5
Stéphane Metz <condor.webdev@gmail.com>
2019-09-22 13:45:54 +0200

    added search function to views

--- store/views.py
+++ store/views.py
@@ -9,3 +9,11 @@ def listing(request):
     albums = ["<li>{}</li>".format(album['name']) for album in ALBUMS]
     message = """<ul>{}</ul>""".format("\n".join(albums))
     return HttpResponse(message)
+
+def detail(request, album_id):
+    id = int(album_id) # make sure we have an integer.
+    album = ALBUMS[id] # get the album with its id.
+    artists = " ".join([artist['name'] for artist in album['artists']]) # grab artists name and create a string out of it.
+    message = "Le nom de l'album est {}. Il a été écrit par {}".format(album['name'], artists)
+    return HttpResponse(message)
+
