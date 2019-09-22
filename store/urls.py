473c84b52a13afa2847deeee1ff63f2e882d80ad
Stéphane Metz <condor.webdev@gmail.com>
2019-09-22 13:20:53 +0200

    routes for store updated in main

--- store/urls.py
+++ store/urls.py
@@ -3,6 +3,4 @@ from django.conf.urls import url
 from . import views # import views so we can use them in urls.
 
 
-urlpatterns = [
-    url(r'^$', views.index), # "/store" will call the method "index" in "views.py"
-]
+urlpatterns = []
