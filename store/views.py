3db3a74e2671e77e9e3176272c0e9ce510b3a93c
Stéphane Metz <condor.webdev@gmail.com>
2019-09-24 15:49:34 +0200

    pagination

--- store/views.py
+++ store/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, get_object_or_404
 
 from .models import Album, Artist, Contact, Booking
@@ -11,9 +12,20 @@ def index(request):
     return render(request, 'store/index.html', context)
 
 def listing(request):
-    albums = Album.objects.filter(available=True)
+    albums_list = Album.objects.filter(available=True)
+    paginator = Paginator(albums_list, 9)
+    page = request.GET.get('page')
+    try:
+        albums = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        albums = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        albums = paginator.page(paginator.num_pages)
     context = {
-        'albums': albums
+        'albums': albums,
+        'paginate': True
     }
     return render(request, 'store/listing.html', context)
 
